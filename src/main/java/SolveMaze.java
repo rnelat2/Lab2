80f54ddf614ca345fd450f0f6bb730b34a4a6681
rachita <rnelat2@illinois.edu>
2018-01-31 18:45:36 -0600

    lab-2

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,14 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            // while (isFinished() == false)}
+            //  maze.turnRight();
+            //  While (!canMove)
+            //  {
+            //   maze.turnLeft();
+            //  }
+            //  maze.move();
+            //}
         }
 
         if (maze.isFinished()) {
